1fb263d14f202cd1983afd7e5c10ff7880a4f8de
Yesseniya Koshmetova <kosmetova77@mail.ru>
2024-06-03 17:03:15 +0500

    new file:   Agas.html

---
+++ source/js/At.js
@@ -0,0 +1,128 @@
+
+// /*=============== SEARCH ===============*/
+
+
+let phrases = [
+    { phrase: "At aryltu", page: "At.html" },
+    { phrase: "Tonnyñ ışkı bauyndai", page: "Tonnyn.html"},
+    { phrase: "Töbesı kökke jetu", page: "Tobesi.html" },
+    { phrase: "Eñbegı janu", page: "Enbegi.html"  },
+    { phrase: "Baiyz tappau", page: "Baiyz.html" },
+    { phrase: "Jüregı tas töbesıne şyğu", page: "Juregi.html" },
+    { phrase: "Aiağy aiağyna jūqpau", page: "Aiagy.html" },
+    { phrase: "Ekı iyğyna ekı kısı mıngendei", page: "Eki.html" },
+    { phrase: "Talağy tars airylu", page: "Talagy.html" },
+    { phrase: "Ağaş atqa mıngızu", page: "Agas.html"  }
+];
+
+function searchByPrefix(prefix) {
+    let results = [];
+    for (let i = 0; i < phrases.length; i++) {
+        let phrase = phrases[i].phrase.toLowerCase();
+        if (phrase.startsWith(prefix.toLowerCase())) {
+            results.push(phrases[i]);
+        }
+    }
+    return results;
+
+}
+
+const searchButton = document.getElementById('search-button'),
+    searchClose = document.getElementById('search-close'),
+    searchContent = document.getElementById('search-content'),
+    searchInput = document.querySelector('.search__input'),
+    searchResults = document.getElementById('search-results');
+
+/*====== SEARCH SHOW ======*/
+/* Validate if constant exists */
+if (searchButton) {
+    searchButton.addEventListener('click', () => {
+        searchContent.classList.add('show-search');
+        searchInput.focus();
+    })
+}
+
+/*====== SEARCH HIDDEN ======*/
+/* Validate if constant exists */
+if (searchClose) {
+    searchClose.addEventListener('click', () => {
+        searchContent.classList.remove('show-search');
+        searchInput.value = '';
+        searchResults.textContent = '';
+    })
+}
+
+/*====== SEARCH FUNCTIONALITY ======*/
+searchInput.addEventListener('input', () => {
+    let prefix = searchInput.value.trim();
+    let searchResultsArray = searchByPrefix(prefix);
+    searchResults.textContent = '';
+    if (searchResultsArray.length > 0) {
+        searchResultsArray.forEach(result => {
+            let resultElement = document.createElement('p');
+            resultElement.textContent = result.phrase;
+            resultElement.addEventListener('click', () => {
+                window.location.href = result.page || '#';
+            });
+            searchResults.appendChild(resultElement);
+        });
+    } else {
+        let noResultsElement = document.createElement('p');
+        noResultsElement.textContent = '';
+        searchResults.appendChild(noResultsElement);
+    }
+
+
+    if (prefix === '') {
+        searchResults.textContent = '';
+    }
+});
+
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    window.scrollY >= -1 ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
+}
+
+window.addEventListener('scroll', shadowHeader)
+
+
+/*=============== SHOW SCROLL UP ===============*/
+
+const scrollUp = () => {
+
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTION ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop = 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        }
+        else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('load', shadowHeader)
+window.addEventListener('scroll', scrollActive)
+
